789301ea2a48fe182c82040145732c6bd93ca05b
Aminat27 <adebayoaminat27@gmail.com>
2022-11-29 11:19:06 +0000

    a program that prints the last digit

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+last_digit = int(repr(number)[-1])
+message = "Last digit of " + f"{number} " + "is " + f"{last_digit}"
+if last_digit > 5 :
+	print(message + " and is greater than 5")
+elif last_digit == 0:
+	print(message + " and is zero")
+elif {last_digit > 6 and not 0}:
+	print(message + " and is less than 6 and not 0")
