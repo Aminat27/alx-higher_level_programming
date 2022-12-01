64d136cf924d4a712f3ce4f50ce2d43e81e197f4
Aminat27 <adebayoaminat27@gmail.com>
2022-12-01 07:34:55 +0000

    a program that does addition

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import add_0
+    from add_0 import add
     a = 1
     b = 2
-    add_0.add(1, 2)
-    print (f"{a} + " f"{b} = " f"{a + b}" )
+
+    print ("{} + {} = {}". format(a, b, add(a, b)) )
