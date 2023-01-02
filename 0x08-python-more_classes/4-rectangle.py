6d62d60d922b56f8aa6bd06ceb3ab98b3cb5eddb
Aminat27 <adebayoaminat27@gmail.com>
2023-01-02 15:47:59 +0100

    Eval is magic

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -1,4 +1,3 @@
-
 #!/usr/bin/python3
 """ A rectangle class"""
 
@@ -50,6 +49,7 @@ class Rectangle:
         if self.__width == 0 or self.__height == 0:
             return 0
         return (2 * (self.__width + self.__height))
+
     def __str__(self):
         """Return the printable representation of the Rectangle
         using # character.
