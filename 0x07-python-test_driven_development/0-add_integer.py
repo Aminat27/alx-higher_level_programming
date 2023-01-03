8e721a2f28ebb6ae08c36a138feb02810893c953
Aminat27 <adebayoaminat27@gmail.com>
2023-01-03 20:55:31 +0100

    added documentation

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,6 +1,19 @@
 #!/usr/bin/python3
+"""
+This module is composed by a function that adds two numbers
+"""
+
+
 def add_integer(a, b=98):
-    """Return the addition of two integers a and b"""
+    """Return the addition of two integers a and b
+    Args:
+        a: first number
+        b: second number
+    Returns:
+        The addition of the two given numbers
+
+    """
+
     if not isinstance(a, int) and not isinstance(a, float):
         raise TypeError("a must be an integer")
 
--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,5 +1,16 @@
 #!/usr/bin/python3
+"""
+This module is composed by a function prints a message
+"""
+
+
 def say_my_name(first_name, last_name=""):
+    """ Function that prints "My name is <first name> <last name>"
+    Args:
+        first_name: first name
+        last_name: last name
+    """
+
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
