8a206868bf3b27280bd627ffcd0191a7e79e2efe
Aminat27 <adebayoaminat27@gmail.com>
2023-01-12 01:23:02 +0100

    fix

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -10,5 +10,6 @@ def read_file(filename=""):
         Exception: when the file can be opened
     """
 
-     with open(filename, encoding="utf-8") as f:
-        print(f.read(), end="")
+    with open(filename, 'r', encoding="utf-8") as f:
+        read_data = f.read()
+        print(read_data, end='')
