8ff1f4f52ec76d1ef85648b271b0a01ad328fa76
Aminat27 <adebayoaminat27@gmail.com>
2023-02-16 21:31:19 +0100

    count 89

---
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -0,0 +1,3 @@
+-- Displays the number of records
+-- Query to display the number of records with id = 89 in a table of the database hbtn_0c_0 in your MySQL server
+SELECT COUNT(*) as id FROM first_table WHERE id=89;
