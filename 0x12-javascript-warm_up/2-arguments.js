96016e9936cc39d7327c1ff09064dd9ceb28fa54
Aminat27 <adebayoaminat27@gmail.com>
2023-03-14 16:08:28 +0100

    prints a message depending of the number of arguments passed

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -7,7 +7,7 @@
    which the js engine runs.
    so process.argv[2] ... contains users passed in arguments.
 */
-if (process.argv.length < 3) {
+if (process.argv.length === 2) {
   console.log('No argument');
 } else if (process.argv.length === 3) {
   console.log('Argument found');
