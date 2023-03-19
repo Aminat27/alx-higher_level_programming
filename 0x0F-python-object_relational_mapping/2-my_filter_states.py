ac7283d34d3627c524cc4269dbe765275e9f430e
Aminat27 <adebayoaminat27@gmail.com>
2023-03-19 11:39:01 +0100

    added 2

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""List all states from the database in ascending order of states"""
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
+# Create a cursor object
+    cursor = db.cursor()
+
+# Define the query to select all states from the table
+    cursor.execute("SELECT * FROM states WHERE name LIKE BINARY '{}'"
+                   .format(sys.argv[4]))
+
+# Fetch all the results
+    results = cursor.fetchall()
+
+# Print the list of states
+    for row in results:
+        print(row)
+
+# Close the cursor and database connections
+    cursor.close()
+    db.close()
