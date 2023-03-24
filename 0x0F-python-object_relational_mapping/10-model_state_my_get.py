bfbbae7c8dfcef44386bbc99e0d03e2bb5205672
Aminat27 <adebayoaminat27@gmail.com>
2023-03-24 16:28:05 +0100

    added 10

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -4,14 +4,15 @@ Script that prints the State object with the name passed as argument
 from the database hbtn_0e_6_usa using SQLAlchemy module.
 """
 
-from sys import argv
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+import sys jmport argv
 from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
 
 if __name__ == '__main__':
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
                            .format(argv[1], argv[2], argv[3]))
+    Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
     state = session.query(State).filter(State.name == argv[4]).first()
