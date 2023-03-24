af5681a2709d029fb65fe0ee520dec50f6b659b8
Aminat27 <adebayoaminat27@gmail.com>
2023-03-24 11:02:21 +0100

    added 10

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -11,12 +11,13 @@ from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
     engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}".
-                           format(*argv[1:4]))
+                           format(argv[1], argv[2], argv[3]))
     Base.metadata.create_all(engine)
     session = sessionmaker(bind=engine)
     session = session()
 
-    state = session.query(State).filter(State.name == (argv[4], )).first()
-    if state is None:
+    state = session.query(State).filter(State.name == (argv[4], ))
+    try:
+        print(state[0].id)
+    except IndexError:
         print("Not found")
-    print("{}".format(state.id))
