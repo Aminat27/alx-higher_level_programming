4f4ea2b0aa1749a50f26e6a670b8de8d3943f7b7
Aminat27 <adebayoaminat27@gmail.com>
2023-03-24 10:57:14 +0100

    added 10

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -5,16 +5,17 @@ from the database hbtn_0e_6_usa using SQLAlchemy module.
 """
 
 from sys import argv
+from model_state import State, Base
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from model_state import Base, State
 
 if __name__ == '__main__':
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
                            .format(argv[1], argv[2], argv[3]))
+    Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    state = session.query(State).filter(State.name == argv[4]).first()
+    state = session.query(State).filter(State.name == argv[4], )).first()
     if state:
         print(state.id)
     else:
