f2226185d63105682cb39d8f8e1b7cc2965b96c6
Aminat27 <adebayoaminat27@gmail.com>
2023-03-24 10:30:37 +0100

    added 9

---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+
+"""
+sunday moses
+a script that lists all State objects staring with a
+"""
+from sys import argv
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}".
+                           format(argv[1], argv[2], argv[3]))
+
+    Base.metadata.create_all(engine)
+    session = sessionmaker(bind=engine)
+    session = session()
+
+    state = session.query(State).filter(State.name.like('%a%'))
+    for name in state:
+        print("{}: {}".format(state.id, state.name))
