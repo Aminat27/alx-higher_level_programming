894d76abd3fbebb11a25ddc0351940734b59c62c
Aminat27 <adebayoaminat27@gmail.com>
2023-03-24 17:11:35 +0100

    added 13

---
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+"""
+Adebayo Ahmed
+This is the State module
+This module handles  Base that inherits from declarative_Base module
+"""
+from sqlalchemy import Integer, Column, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class State(Base):
+    """
+    This class connects/relates to the states table in our
+    hbtn_0e_4_usa database
+    Args:
+        @id: ud of the element in the state table
+        @name: name of eqch state in the table
+    """
+
+    __tablename__ = 'states'
+    id = Column(Integer, autoincrement=True, nullable=False, primary_key=True)
+    name = Column(String(128), nullable=False)
