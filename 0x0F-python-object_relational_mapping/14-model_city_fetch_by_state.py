793ac6721e52c102374a1ab9e1c12a0cf56d6c17
Aminat27 <adebayoaminat27@gmail.com>
2023-03-25 12:04:34 +0100

    fixed

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -11,15 +11,16 @@ from sqlalchemy.orm import sessionmaker
 
 
 if __name__ == '__main__':
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
-                           format(sys.argv[1], sys.argv[2], sys.argv[3]),
+    user = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(user, password, db_name),
                            pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    st_cty = session.query(State, City).filter(State.id == City.state_id)
-
-    for state, city in st_cty:
+    for state, city in session.query(State, City)\
+            .filter(State.id == City.state_id).order_by(City.id).all():
         print("{}: ({}) {}".format(state.name, city.id, city.name))
-
-    session.close()
--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -1,24 +1,21 @@
 #!/usr/bin/python3
 
 """
-Adebayo Ahmed
 This is the State module
 This module handles  Base that inherits from declarative_Base module
 """
 
 from sqlalchemy import Integer, String, Column, ForeignKey
+from sqlalchemy.orm import relationship
 from model_state import Base
 
 
 class City(Base):
-    """ingerits from State class
-    Args:
-        @id: int
-        @name: str
-        @state_id: int
-    """
+    """City class definition"""
+
+
     __tablename__ = "cities"
-    id = Column(Integer, unique=True, nullable=False, primary_key=True)
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
     name = Column(String(128), nullable=False)
     state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
-
+    state = relationship("State", back_populates="cities")
