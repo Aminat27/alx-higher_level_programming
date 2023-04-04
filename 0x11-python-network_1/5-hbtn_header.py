d1fbcc717ca33aa61ec9a337682fb8e1845cff1f
Aminat27 <adebayoaminat27@gmail.com>
2023-04-04 22:33:16 +0100

    5

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
-
-"""a Python script that fetches https://alx-intranet.hbtn.io/status"""
+"""
+Python script that sends a request to the URL and
+displays the value of a variable in the response header
+"""
 import requests
 import sys
 
 
 if __name__ == "__main__":
-    url = sys.argv[1]
-
     try:
-        response = requests.get(url)
-        x_request_id = response.headers(['X-Request-Id'])
-        print(x_request_id)
+        r = requests.get(sys.argv[1])
+        print(r.headers['X-Request-Id'])
     except:
         pass
+
