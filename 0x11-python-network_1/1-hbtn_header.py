b082ffbda42679f02ab74508f895d60dc4b6d23b
Aminat27 <adebayoaminat27@gmail.com>
2023-04-04 09:20:50 +0100

    added 0

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+"""a Python script that fetches https://alx-intranet.hbtn.io/status"""
+import urllib.request
+import sys
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    with urllib.request.urlopen(url) as response:
+        html = response.info()
+        print(headers['X-Request-Id'])
