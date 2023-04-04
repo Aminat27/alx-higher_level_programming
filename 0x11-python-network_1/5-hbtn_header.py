e82a2467ff7166bd854b0745e35ded82063ef1b7
Aminat27 <adebayoaminat27@gmail.com>
2023-04-04 22:28:22 +0100

    5

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -8,10 +8,9 @@ import sys
 if __name__ == "__main__":
     url = sys.argv[1]
 
-    response = requests.get(url)
-    x_request_id = response.headers.get('X-Request-Id')
-
-    if x_request_id:
+    try:
+        response = requests.get(url)
+        x_request_id = response.headers(['X-Request-Id'])
         print(f"X-Request-Id: {x_request_id}")
-    else:
-        print("No X-Request-Id header found in the response.")
+    except:
+        pass
