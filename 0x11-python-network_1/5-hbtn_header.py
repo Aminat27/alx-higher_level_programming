522ca8a1dcc4ebbdc4eec88c5457b381023d67fb
Aminat27 <adebayoaminat27@gmail.com>
2023-04-04 22:31:00 +0100

    5

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -11,6 +11,6 @@ if __name__ == "__main__":
     try:
         response = requests.get(url)
         x_request_id = response.headers(['X-Request-Id'])
-        print(f"X-Request-Id: {x_request_id}")
+        print(x_request_id)
     except:
         pass
