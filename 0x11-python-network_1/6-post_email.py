e1a7ae5895f82561decc5b4129cdd821b439e152
Aminat27 <adebayoaminat27@gmail.com>
2023-04-04 22:39:01 +0100

    6

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+"""Python script"""
+
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    email = sys.argv[2]
+
+    payload = {'email': email}
+    response = requests.post(url, data=payload)
+
+    print("Response body:")
+    print(response.text)
